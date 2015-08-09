192f6d0ee5465fd86402acb835631d7028c0b060
James Elías <jamiees2@gmail.com>
2015-08-09 22:05:03 +0000

    fixed master challenges

--- api/achievements/masterchallenge/masterchallenge.py
+++ api/achievements/masterchallenge/masterchallenge.py
@@ -1,5 +1,11 @@
 def process(api, data):
     pid = data["pid"]
-    pid_map = api.stats.get_pid_categories()
-    category = pid_map[pid]
-    return category == "Master Challenge", {}
+    master_challenges = {
+        "sausages": "bf9cee6ef204cf043be970dcd4837fa4",
+        "giga": "5262457500d337190c0f3484701a4ca6",
+        "mondrian": "db7ef43550856247226e9b1423671b77",
+        "wiki2": "ccb44839ee47cdf92c3f55c3e596801c",
+        "alternate_rsa": "4f902c78957792f1fa6552719ff40997",
+        "supernote": "13ce1fe752f4ad502bab81c312bf806b"
+    }
+    return pid in master_challenges.values()
