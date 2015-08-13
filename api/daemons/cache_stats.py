1d7258cf19442510096292a3668d49738487f8cf
James Elías <jamiees2@gmail.com>
2015-08-13 21:37:51 +0000

    fixed things

--- api/daemons/cache_stats.py
+++ api/daemons/cache_stats.py
@@ -9,9 +9,10 @@ def cache(f, *args, **kwargs):
     api.cache.set(key, result)
 
 def run():
-    print("Caching the public scoreboard entries...")
+    print("Caching the contestant scoreboard entries...")
     cache(api.stats.get_all_team_scores)
+    print("Caching the total scoreboard entries...")
+    cache(api.stats.get_all_team_scores, show_ineligible=True)
     print("Caching the contestant scoreboard graph...")
     cache(api.stats.get_top_teams_score_progressions)
     print("Caching the total scoreboard graph...")
-    cache(api.stats.get_top_teams_score_progressions, show_ineligible=True)
